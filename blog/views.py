1682e98d065b371b1f4673a7d120c0e0a647865e
Sachin <sachnaror@gmail.com>
2024-10-14 23:19:05 +0530

    The diligent mind reaps bountiful knowledge

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import get_object_or_404, render
+
+from .models import Blog
+
+
+def blog_list(request):
+    blogs = Blog.objects.filter(status='published').order_by('-timestamp')
+    return render(request, 'blog_list.html', {'blogs': blogs})
+
+def blog_detail(request, id):
+    blog = get_object_or_404(Blog, id=id, status='published')
+    return render(request, 'blog_detail.html', {'blog': blog})
