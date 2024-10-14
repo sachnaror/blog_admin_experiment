9792d29c370c7e052e74a600176d2c0645e30af0
Sachin <sachnaror@gmail.com>
2024-10-14 23:12:27 +0530

    Hard work: the architect of dreams

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'timestamp')
+    list_filter = ('status', 'timestamp')
+    search_fields = ('title', 'content')
+    ordering = ('-timestamp',)
+    actions = ['publish_blogs', 'unpublish_blogs']
+
+    def publish_blogs(self, request, queryset):
+        queryset.update(status='published')
+    publish_blogs.short_description = "Publish selected blogs"
+
+    def unpublish_blogs(self, request, queryset):
+        queryset.update(status='draft')
+    unpublish_blogs.short_description = "Unpublish selected blogs"
